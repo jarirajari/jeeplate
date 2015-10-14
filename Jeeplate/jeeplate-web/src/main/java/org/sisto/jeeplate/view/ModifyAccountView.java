a2107f07ce57567a05818afc9e5a5ebe7a4437cd
Jari Kuusisto <mr.jari.kuusisto@gmail.com>
2015-10-15 00:25:06 +0300

    Started implementing user account settings features: created basic dialog that needs data and action functionalities still.

--- Jeeplate/jeeplate-web/src/main/java/org/sisto/jeeplate/view/ModifyAccountView.java
+++ Jeeplate/jeeplate-web/src/main/java/org/sisto/jeeplate/view/ModifyAccountView.java
@@ -19,15 +19,19 @@
 package org.sisto.jeeplate.view;
  
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import org.primefaces.context.RequestContext;
 import org.sisto.jeeplate.domain.user.UserData;
 
 @Named @ViewScoped
 public class ModifyAccountView extends AbstractView implements Serializable {
-
+    private String domain;
     // name
     private String screenName;
     private String firstName;
@@ -44,13 +48,149 @@ public class ModifyAccountView extends AbstractView implements Serializable {
     private String country;
     private String city;
     private String timezone;
-    // other
     
     @Inject 
     private UserData user;
 
     @PostConstruct
     public void init() {
+        this.screenName = "";
+        this.firstName = "";
+        this.middleName = "";
+        this.lastName = "";
+        this.streetAddress = "";
+        this.postalCode = "";
+        this.mobileNumber = "";
+        this.emailAddress = "";
+        this.language = "";
+        this.country = "";
+        this.city = "";
+        this.timezone = "";
+    }
+
+    public String getScreenName() {
+        return screenName;
+    }
+
+    public void setScreenName(String screenName) {
+        this.screenName = screenName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTimezone() {
+        return timezone;
+    }
+
+    public void setTimezone(String timezone) {
+        this.timezone = timezone;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+    
+    public String completeDomain(String joinCodeHash) {
+        String domainThatMachesCode = "";
         
+        // system must hold a map of domains key=joinCodeHash, value=domainFqdnKey
+        // domainFqdnKey is used to find out the "id" of the domain that user joins
+        // domainThatMachesCode = 
+         
+        return domainThatMachesCode;
+    }
+    
+    public void modify() {
+        System.out.println("modify!");
+        this.user.findLoggedInUser(this.currentUser());
+        Boolean changed = Boolean.TRUE;
+        
+        if (changed) {
+            RequestContext.getCurrentInstance().execute("PF('accountDlg').hide()");
+        } else {
+            this.showFacesMessage(FacesMessage.SEVERITY_INFO, "NOT OK, not modify account");
+        }
     }
 }
